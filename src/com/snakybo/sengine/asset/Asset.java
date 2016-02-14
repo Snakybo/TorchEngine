381bdd3a9f4dd44621ce08bbafc77e111332b243
Kevin Krol <Krol.kevin@gmail.com>
2016-02-14 01:33:06 +0100

    Added an asset and asset parser

---
+++ src/com/snakybo/sengine/asset/Asset.java
@@ -0,0 +1,79 @@
+package com.snakybo.sengine.asset;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.snakybo.sengine.debug.Logger;
+import com.snakybo.sengine.io.FileUtils;
+import com.snakybo.sengine.object.Object;
+
+/**
+ * @author Kevin
+ * @since Feb 13, 2016
+ */
+public abstract class Asset extends Object
+{
+	public Asset()
+	{
+		super("Asset");
+		setName(getClass().getSimpleName());
+	}
+	
+	/**
+	 * Load an asset from a file
+	 * @param asset - The type of the asset
+	 * @param path - The path to the asset data
+	 * @return An asset with the data read from the specified path
+	 */
+	public static <T extends Asset> T load(Class<T> asset, String path)
+	{
+		Logger.log("Loading asset at: " + path, "Asset");
+		
+		Path file = FileUtils.load(path, "asset", false);		
+		if(file == null)
+		{
+			Logger.logError("No asset found at: " + path, "Asset");
+			return null;
+		}
+		
+		try
+		{
+			Asset result = asset.newInstance();			
+			AssetParser.setFields(result, AssetParser.getFields(file));
+			
+			return asset.cast(result);
+		}
+		catch(InstantiationException | IllegalAccessException e)
+		{
+			Logger.logException(e, "AssetParser");
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Write an asset's contents to the disk
+	 * @param asset - The asset to save
+	 * @param path - The path to save the asset to
+	 */
+	public static void save(Asset asset, String path)
+	{
+		Logger.log("Saving asset: " + asset + " at: " + path, "Asset");
+		
+		List<String> lines = new ArrayList<String>();
+		Path file = FileUtils.load(path, "asset");
+		
+		// Add fields
+		lines.add("fields:");		
+		Map<String, String> fields = AssetParser.getFields(asset);
+		for(Map.Entry<String, String> field : fields.entrySet())
+		{
+			lines.add("    " + field.getKey() + ": " + field.getValue());
+		}
+		
+		// Write the data
+		FileUtils.write(file, lines);
+	}
+}
---
+++ src/com/snakybo/sengine/asset/AssetParser.java
@@ -0,0 +1,79 @@
+package com.snakybo.sengine.asset;
+
+import java.lang.reflect.Field;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.snakybo.sengine.io.FileUtils;
+import com.snakybo.sengine.reflection.FieldUtilities;
+
+/**
+ * @author Kevin
+ * @since Feb 13, 2016
+ */
+public final class AssetParser
+{
+	private AssetParser()
+	{
+		throw new AssertionError();
+	}
+	
+	public static void setFields(Asset asset, Map<String, String> fields)
+	{
+		for(Map.Entry<String, String> target : fields.entrySet())
+		{
+			Field field = FieldUtilities.getAccessibleField(asset, target.getKey());
+			
+			if(field != null)
+			{
+				boolean accessible = field.isAccessible();
+				field.setAccessible(true);
+				
+				FieldUtilities.setAccessibleFieldValue(asset, field, target.getValue());
+				
+				field.setAccessible(accessible);
+			}
+		}
+	}
+	
+	public static Map<String, String> getFields(Asset asset)
+	{
+		Map<String, String> result = new HashMap<String, String>();
+		List<Field> fields = FieldUtilities.getAccessibleFields(asset);
+		
+		for(Field field : fields)
+		{
+			String name = field.getName();
+			Object value = FieldUtilities.getValue(asset, field);
+			
+			result.put(name, value == null ? "" : value.toString());
+		}
+		
+		return result;
+	}
+	
+	public static Map<String, String> getFields(Path file)
+	{
+		Map<String, String> fields = new HashMap<String, String>();
+		List<String> lines = FileUtils.read(file);
+		
+		// Get all field data
+		int fieldsStart = lines.indexOf("fields:");		
+		for(int i = fieldsStart; i < lines.size(); i++)
+		{
+			String line = lines.get(i);
+			
+			if(!line.startsWith("    "))
+			{
+				continue;
+			}
+			
+			String[] fieldData = line.split(":");
+			fields.put(fieldData[0].trim(), fieldData[1].trim());
+		}
+		
+		return fields;
+	}
+}
