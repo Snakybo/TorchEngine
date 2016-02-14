f1ffdbdad4c62899578e660fb30975df81f093da
Kevin Krol <Krol.kevin@gmail.com>
2016-02-14 02:18:53 +0100

    Fixed javadoc in AccessibleField

--- src/com/snakybo/sengine/annotations/AccessibleField.java
+++ src/com/snakybo/sengine/annotations/AccessibleField.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Add this annotation before a private, protected or internal field to expose it to the engine
+ * @author Kevin
+ * @since Feb 13, 2016
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface AccessibleField
