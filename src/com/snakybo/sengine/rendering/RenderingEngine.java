d8873a6f9cedcf1c8f1c2e0e97aaf6d03024f6da
Kevin Krol <Krol.kevin@gmail.com>
2016-02-17 18:03:09 +0100

    Added skeleton rendering engine

---
+++ src/com/snakybo/sengine/rendering/RenderingEngine.java
@@ -0,0 +1,23 @@
+package com.snakybo.sengine.rendering;
+
+/**
+ * @author Kevin
+ * @since Feb 14, 2016
+ */
+public final class RenderingEngine
+{
+	static
+	{
+		
+	}
+	
+	private RenderingEngine()
+	{
+		throw new AssertionError();
+	}
+	
+	public static void renderCycle()
+	{
+		
+	}
+}
