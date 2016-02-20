6201004e85731baee4d8bb6c81f6e24b74a229a2
Kevin Krol <Krol.kevin@gmail.com>
2016-02-20 15:27:18 +0100

    Cleaned up AudioListener

--- src/com/snakybo/sengine/audio/AudioListener.java
+++ src/com/snakybo/sengine/audio/AudioListener.java
@@ -1,10 +1,14 @@
 package com.snakybo.sengine.audio;
 
+import static org.lwjgl.openal.AL10.AL_ORIENTATION;
+import static org.lwjgl.openal.AL10.AL_POSITION;
+import static org.lwjgl.openal.AL10.AL_VELOCITY;
+import static org.lwjgl.openal.AL10.alListenerfv;
+
 import java.nio.FloatBuffer;
 
 import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
-import static org.lwjgl.openal.AL10.*;
 
 import com.snakybo.sengine.debug.Logger;
 import com.snakybo.sengine.object.Component;
@@ -18,17 +22,13 @@ public class AudioListener extends Component
 {	
 	static AudioListener instance;
 	
-	private FloatBuffer positionBuffer;
-	private FloatBuffer velocityBuffer;
-	private FloatBuffer orientationBuffer;
-	
 	private Vector3f lastPosition;
 	
 	public AudioListener()
 	{
 		if(instance != null)
 		{
-			Logger.logWarning("There's already an audio listener in the scene", this);
+			Logger.logWarning("There already is an audio listener in the scene", this);
 			return;
 		}
 		
@@ -38,46 +38,17 @@ public class AudioListener extends Component
 	@Override
 	protected void start()
 	{
-		lastPosition = getTransform().getPosition();		
-		positionBuffer = BufferUtils.createFloatBuffer(3);
-		velocityBuffer = BufferUtils.createFloatBuffer(3);
-		orientationBuffer = BufferUtils.createFloatBuffer(6);
+		lastPosition = getTransform().getPosition();
 	}
 	
 	@Override
 	protected void postUpdate()
 	{
-		// Listener position
-		Vector3f position = getTransform().getPosition();
-		
-		positionBuffer.clear();
-		positionBuffer.put(new float[] { position.x, position.y, position.z });
-		positionBuffer.rewind();
-		
-		// Listener velocity
-		float dt = Time.getDeltaTime();
-		Vector3f velocity = getTransform().getPosition().sub(lastPosition, new Vector3f()).sub(dt, dt, dt);
-		
-		velocityBuffer.clear();
-		velocityBuffer.put(new float[] { velocity.x, velocity.y, velocity.z });
-		velocityBuffer.rewind();
-		
-		// Listener orientation
-		//Vector3f forward = getTransform().getForward();
-		Vector3f forward = getTransform().getBack();
-		Vector3f up = getTransform().getUp();
-		
-		orientationBuffer.clear();
-		orientationBuffer.put(new float[] { forward.x, forward.y, forward.z, up.x, up.y, up.z });
-		//orientationBuffer.put(new float[] { 0, 0, 1, 0, 1, 0 });
-		orientationBuffer.rewind();
+		setPosition();
+		setVelocity();
+		setOrientation();
 		
 		lastPosition = getTransform().getPosition();
-		
-		// Set listener values
-		alListenerfv(AL_POSITION, positionBuffer);
-		alListenerfv(AL_VELOCITY, velocityBuffer);
-		alListenerfv(AL_ORIENTATION, orientationBuffer);
 	}
 	
 	@Override
@@ -87,5 +58,49 @@ public class AudioListener extends Component
 		{
 			instance = null;
 		}
+	}
+	
+	/**
+	 * Set the position of the listener
+	 */
+	private void setPosition()
+	{
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(3);		
+		Vector3f position = getTransform().getPosition();
+		
+		alListenerfv(AL_POSITION, position.get(buffer));
+	}
+	
+	/**
+	 * Set the velocity of the listener
+	 */
+	private void setVelocity()
+	{
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(3);
+		
+		float dt = Time.getDeltaTime();
+		Vector3f position = getTransform().getPosition();
+		Vector3f velocity = position.sub(lastPosition, new Vector3f());
+		velocity.sub(dt, dt, dt);
+		
+		alListenerfv(AL_VELOCITY, velocity.get(buffer));
+	}
+	
+	/**
+	 * Set the orientation of the listener
+	 */
+	private void setOrientation()
+	{
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(6);
+		
+		// TODO: Fix listener orientation (?)
+		//Vector3f forward = getTransform().getForward();
+		Vector3f forward = getTransform().getBack();
+		Vector3f up = getTransform().getUp();
+		
+		forward.get(buffer);
+		up.get(buffer);
+		
+		alListenerfv(AL_ORIENTATION, buffer);
 	}
 }
