3663b110c42e0efdcb90fe0d3524176ac818fcb1
Kevin Krol <Krol.kevin@gmail.com>
2016-02-20 14:58:32 +0100

    Removed old Asset and AssetParser, and added an AssetDatabase

--- src/com/snakybo/sengine/asset/Asset.java
+++ src/com/snakybo/sengine/asset/Asset.java
@@ -1,76 +1,13 @@
 package com.snakybo.sengine.asset;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import com.snakybo.sengine.debug.Logger;
-import com.snakybo.sengine.io.File;
-import com.snakybo.sengine.object.Object;
+import com.snakybo.sengine.util.IDestroyable;
 
 /**
  * @author Snakybo
  * @since 1.0
  */
-public abstract class Asset extends Object
+public abstract class Asset implements IDestroyable
 {
-	public Asset()
-	{
-		super("Asset");
-		setName(getClass().getSimpleName());
-	}
-	
-	/**
-	 * Load an asset from a file
-	 * @param asset - The type of the asset
-	 * @param path - The path to the asset data
-	 * @return An asset with the data read from the specified path
-	 */
-	public static <T extends Asset> T load(Class<T> asset, String path)
-	{
-		Logger.log("Loading asset at: " + path, "Asset");
-				
-		if(!File.exists(path + ".asset"))
-		{
-			Logger.logError("No asset found at: " + path, "Asset");
-			return null;
-		}
-		
-		try
-		{
-			Asset result = asset.newInstance();			
-			AssetParser.setFields(result, AssetParser.getFields(path + ".asset"));
-			
-			return asset.cast(result);
-		}
-		catch(InstantiationException | IllegalAccessException e)
-		{
-			Logger.logException(e, "AssetParser");
-		}
-		
-		return null;
-	}
-	
-	/**
-	 * Write an asset's contents to the disk
-	 * @param asset - The asset to save
-	 * @param path - The path to save the asset to
-	 */
-	public static void save(Asset asset, String path)
-	{
-		Logger.log("Saving asset: " + asset + " at: " + path, "Asset");
-		
-		List<String> lines = new ArrayList<String>();
-		
-		// Add fields
-		lines.add("fields:");		
-		Map<String, String> fields = AssetParser.getFields(asset);
-		for(Map.Entry<String, String> field : fields.entrySet())
-		{
-			lines.add("    " + field.getKey() + ": " + field.getValue());
-		}
-		
-		// Write the data
-		File.writeLines(path + ".asset", lines);
-	}
+	@Override
+	public abstract void destroy();
 }
---
+++ src/com/snakybo/sengine/asset/AssetDatabase.java
@@ -0,0 +1,102 @@
+package com.snakybo.sengine.asset;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Snakybo
+ * @since 1.0
+ */
+public final class AssetDatabase
+{
+	private static Map<String, Integer> useCount = new HashMap<String, Integer>();
+	private static Map<String, Asset> assets = new HashMap<String, Asset>();
+	
+	private static Set<Object> sources = new HashSet<Object>();
+	
+	private AssetDatabase()
+	{
+		throw new AssertionError();
+	}
+	
+	/**
+	 * Register a new asset, doing this will add the {@code asset} to the database
+	 * @param assetName - The name of the asset
+	 * @param asset - The asset to register
+	 * @param source - The source of the registration
+	 * @return The {@code asset}
+	 */
+	public static Asset register(String assetName, Asset asset, Object source)
+	{
+		if(!useCount.containsKey(assetName))
+		{
+			useCount.put(assetName, 0);
+			assets.put(assetName, asset);
+		}
+		
+		return link(assetName, source);
+	}
+	
+	/**
+	 * Link an object to an {@link Asset}, this will increase the usage-counter by 1
+	 * @param assetName - The name of the asset
+	 * @param source - The object to link to the asset
+	 * @return The {@link Asset} registered with the same {@code assetName}
+	 */
+	public static Asset link(String assetName, Object source)
+	{
+		if(useCount.containsKey(assetName))
+		{
+			int usages = useCount.get(assetName);
+			useCount.put(assetName, usages + 1);			
+			sources.add(source);
+			
+			return assets.get(assetName);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Unlink an object from an {@link Asset}, this wil decrease the usage-counter by 1.
+	 * If the usage counter becomes 0 as a result of this operation, it will automatically destroy the asset
+	 * @param assetName - The name of the asset
+	 * @param source - The object to unlink
+	 */
+	public static void unlink(String assetName, Object source)
+	{
+		if(sources.contains(source) && useCount.containsKey(assetName))
+		{
+			int usages = useCount.get(assetName);
+			useCount.put(assetName, usages - 1);
+			
+			// Delete the asset if neccecary
+			if(usages - 1 <= 0)
+			{
+				Asset asset = assets.get(assetName);
+				
+				useCount.remove(assetName);
+				assets.remove(assetName);
+				
+				asset.destroy();
+			}
+		}
+	}
+	
+	/**
+	 * Check whether or not an {@link Asset} has already been registered
+	 * @param assetName - The name of the {@link Asset}
+	 * @return Whether or not the {@link Asset} has been registered
+	 */
+	public static boolean hasAsset(String assetName)
+	{
+		if(assets.containsKey(assetName))
+		{
+			return true;
+		}
+		
+		return false;
+	}
+}
