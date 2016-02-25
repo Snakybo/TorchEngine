993b1b1ba8880e7086ac9f4004531562c00d2134
Kevin Krol <Krol.kevin@gmail.com>
2016-02-25 18:47:04 +0100

    Added sourceless variations to the Logger

--- src/com/snakybo/sengine/debug/Logger.java
+++ src/com/snakybo/sengine/debug/Logger.java
@@ -16,12 +16,30 @@ public final class Logger
 	/**
 	 * Logs a message to the console
 	 * @param b - The boolean to log
+	 */
+	public static void log(boolean b)
+	{
+		log(b, null);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param b - The boolean to log
 	 * @param source - The source of the message, can also be a string
 	 */
 	public static void log(boolean b, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(b);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param c - The char to log
+	 */
+	public static void log(char c)
+	{
+		log(c, null);
 	}
 	
 	/**
@@ -31,8 +49,17 @@ public final class Logger
 	 */
 	public static void log(char c, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(c);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param i - The int to log
+	 */
+	public static void log(int i)
+	{
+		log(i, null);
 	}
 	
 	/**
@@ -42,8 +69,17 @@ public final class Logger
 	 */
 	public static void log(int i, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(i);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param f - The float to log
+	 */
+	public static void log(float f)
+	{
+		log(f, null);
 	}
 	
 	/**
@@ -53,8 +89,17 @@ public final class Logger
 	 */
 	public static void log(float f, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(f);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param d - The double to log
+	 */
+	public static void log(double d)
+	{
+		log(d, null);
 	}
 	
 	/**
@@ -64,8 +109,17 @@ public final class Logger
 	 */
 	public static void log(double d, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(d);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param l - The long to log
+	 */
+	public static void log(long l)
+	{
+		log(l, null);
 	}
 	
 	/**
@@ -75,8 +129,17 @@ public final class Logger
 	 */
 	public static void log(long l, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(l);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param s - The char array to log
+	 */
+	public static void log(char[] s)
+	{
+		log(s, null);
 	}
 	
 	/**
@@ -86,8 +149,17 @@ public final class Logger
 	 */
 	public static void log(char[] s, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(s);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param s - The string to log
+	 */
+	public static void log(String s)
+	{		
+		log(s, null);
 	}
 	
 	/**
@@ -97,8 +169,17 @@ public final class Logger
 	 */
 	public static void log(String s, Object source)
 	{		
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(s);
+	}
+	
+	/**
+	 * Logs a message to the console
+	 * @param o - The object to log
+	 */
+	public static void log(Object o)
+	{
+		log(o, null);
 	}
 	
 	/**
@@ -108,8 +189,17 @@ public final class Logger
 	 */
 	public static void log(Object o, Object source)
 	{
-		logPrefix(LogLevel.Message, source);
+		logPrefix(LogLevel.Message, source, System.out);
 		System.out.println(o);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param b - The boolean to log
+	 */
+	public static void logWarning(boolean b)
+	{
+		logWarning(b, null);
 	}
 	
 	/**
@@ -119,8 +209,17 @@ public final class Logger
 	 */
 	public static void logWarning(boolean b, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(b);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param c - The char to log
+	 */
+	public static void logWarning(char c)
+	{
+		logWarning(c, null);
 	}
 	
 	/**
@@ -130,8 +229,17 @@ public final class Logger
 	 */
 	public static void logWarning(char c, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(c);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param i - The int to log
+	 */
+	public static void logWarning(int i)
+	{
+		logWarning(i, null);
 	}
 	
 	/**
@@ -141,8 +249,17 @@ public final class Logger
 	 */
 	public static void logWarning(int i, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(i);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param f - The float to log
+	 */
+	public static void logWarning(float f)
+	{
+		logWarning(f, null);
 	}
 	
 	/**
@@ -152,8 +269,17 @@ public final class Logger
 	 */
 	public static void logWarning(float f, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(f);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param d - The double to log
+	 */
+	public static void logWarning(double d)
+	{
+		logWarning(d, null);
 	}
 	
 	/**
@@ -163,8 +289,17 @@ public final class Logger
 	 */
 	public static void logWarning(double d, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(d);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param l - The long to log
+	 */
+	public static void logWarning(long l)
+	{
+		logWarning(l, null);
 	}
 	
 	/**
@@ -174,8 +309,17 @@ public final class Logger
 	 */
 	public static void logWarning(long l, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(l);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param s - The char array to log
+	 */
+	public static void logWarning(char[] s)
+	{
+		logWarning(s, null);
 	}
 	
 	/**
@@ -185,8 +329,17 @@ public final class Logger
 	 */
 	public static void logWarning(char[] s, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(s);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param s - The string to log
+	 */
+	public static void logWarning(String s)
+	{		
+		logWarning(s, null);
 	}
 	
 	/**
@@ -196,8 +349,17 @@ public final class Logger
 	 */
 	public static void logWarning(String s, Object source)
 	{		
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(s);
+	}
+	
+	/**
+	 * Logs a warning to the console
+	 * @param o - The object to log
+	 */
+	public static void logWarning(Object o)
+	{
+		logWarning(o, null);
 	}
 	
 	/**
@@ -207,8 +369,17 @@ public final class Logger
 	 */
 	public static void logWarning(Object o, Object source)
 	{
-		logPrefix(LogLevel.Warning, source);
+		logPrefix(LogLevel.Warning, source, System.out);
 		System.out.println(o);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param b - The boolean to log
+	 */
+	public static void logError(boolean b)
+	{
+		logError(b, null);
 	}
 	
 	/**
@@ -225,12 +396,30 @@ public final class Logger
 	/**
 	 * Logs an error to the console
 	 * @param c - The char to log
+	 */
+	public static void logError(char c)
+	{
+		logError(c, null);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param c - The char to log
 	 * @param source - The source of the error, can also be a string
 	 */
 	public static void logError(char c, Object source)
 	{
 		logPrefix(LogLevel.Error, source, System.err);
 		System.err.println(c);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param i - The int to log
+	 */
+	public static void logError(int i)
+	{
+		logError(i, null);
 	}
 	
 	/**
@@ -247,12 +436,30 @@ public final class Logger
 	/**
 	 * Logs an error to the console
 	 * @param f - The float to log
+	 */
+	public static void logError(float f)
+	{
+		logError(f, null);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param f - The float to log
 	 * @param source - The source of the error, can also be a string
 	 */
 	public static void logError(float f, Object source)
 	{
 		logPrefix(LogLevel.Error, source, System.err);
 		System.err.println(f);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param d - The double to log
+	 */
+	public static void logError(double d)
+	{
+		logError(d, null);
 	}
 	
 	/**
@@ -269,12 +476,30 @@ public final class Logger
 	/**
 	 * Logs an error to the console
 	 * @param l - The long to log
+	 */
+	public static void logError(long l)
+	{
+		logError(l, null);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param l - The long to log
 	 * @param source - The source of the error, can also be a string
 	 */
 	public static void logError(long l, Object source)
 	{
 		logPrefix(LogLevel.Error, source, System.err);
 		System.err.println(l);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param s - The char array to log
+	 */
+	public static void logError(char[] s)
+	{
+		logError(s, null);
 	}
 	
 	/**
@@ -291,6 +516,15 @@ public final class Logger
 	/**
 	 * Logs an error to the console
 	 * @param s - The string to log
+	 */
+	public static void logError(String s)
+	{		
+		logError(s, null);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param s - The string to log
 	 * @param source - The source of the error, can also be a string
 	 */
 	public static void logError(String s, Object source)
@@ -302,12 +536,30 @@ public final class Logger
 	/**
 	 * Logs an error to the console
 	 * @param o - The object to log
+	 */
+	public static void logError(Object o)
+	{
+		logError(o, null);
+	}
+	
+	/**
+	 * Logs an error to the console
+	 * @param o - The object to log
 	 * @param source - The source of the error, can also be a string
 	 */
 	public static void logError(Object o, Object source)
 	{
-		logPrefix(LogLevel.Error, source);
+		logPrefix(LogLevel.Error, source, System.err);
 		System.out.println(o);
+	}
+	
+	/**
+	 * Logs an exception
+	 * @param e - The exception to log
+	 */
+	public static void logException(Exception e)
+	{
+		logException(e, null);
 	}
 	
 	/**
@@ -319,18 +571,6 @@ public final class Logger
 	{
 		logPrefix(LogLevel.Exception, source, System.err);
 		e.printStackTrace();
-	}
-	
-	/**
-	 * Log the prefix
-	 * @param level - The desired log level, {@link LogLevel#Message}, {@link LogLevel#Warning}, {@link LogLevel#Error} or {@link LogLevel#Exception}
-	 * @param source - The source of the log
-	 * @see LogLevel
-	 * @see #logPrefix(LogLevel, Object, PrintStream)
-	 */
-	private static void logPrefix(LogLevel level, Object source)
-	{
-		logPrefix(level, source, System.out);
 	}
 	
 	/**
