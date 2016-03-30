1ef9f0e5cebd71a4e07056c3d13e6082f43a004a
Kevin Krol <Krol.kevin@gmail.com>
2016-03-30 20:36:16 +0200

    Added licence

--- src/com/snakybo/sengine/io/FileUtils.java
+++ src/com/snakybo/sengine/io/FileUtils.java
@@ -1,7 +1,32 @@
+// The MIT License(MIT)
+//
+// Copyright(c) 2016 Kevin Krol
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package com.snakybo.sengine.io;
 
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import com.snakybo.sengine.debug.Logger;
 
 /**
  * @author Snakybo
@@ -15,17 +40,50 @@ public final class FileUtils
 	}
 	
 	/**
+	 * Get the file name and extension from a file
+	 * @param file - The file
+	 * @return The file extension
+	 */
+	public static String getFileNameAndExtension(String file)
+	{
+		if(File.exists(file) && !File.isDirectory(file))
+		{
+			Path path = Paths.get(file);
+			return path.getFileName().toString();
+		}
+		
+		Logger.logException(new FileNotFoundException(file), "FileUtils");
+		return "";
+	}
+	
+	/**
+	 * Get the file name from a file
+	 * @param file - The file
+	 * @return The file extension, if there was an error, it will return an empty string
+	 */
+	public static String getFileName(String file)
+	{
+		String fileName = getFileNameAndExtension(file);
+		
+		if(fileName.length() > 0)
+		{
+			return fileName.substring(0, fileName.lastIndexOf('.'));
+		}
+		
+		return "";
+	}
+	
+	/**
 	 * Get the file extension from a file
 	 * @param file - The file
 	 * @return The file extension, if there was an error, it will return an empty string
 	 */
 	public static String getFileExtension(String file)
 	{
-		if(File.exists(file) && !File.isDirectory(file))
+		String fileName = getFileNameAndExtension(file);
+		
+		if(fileName.length() > 0)
 		{
-			Path path = Paths.get(file);
-			String fileName = path.getFileName().toString();
-			
 			return fileName.substring(fileName.lastIndexOf('.') + 1);
 		}
 		
