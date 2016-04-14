e2064e8bea6cad084943ae40e72cd2b2a2f04ca1
Kevin Krol <Krol.kevin@gmail.com>
2016-04-15 00:21:38 +0200

    Revert "Test Gradle with compiler errors"
    This reverts commit 0b308d2a823c9d14459b6c417d21ea30bc71ab96.

--- src/com/snakybo/sengine/SEngine.java
+++ src/com/snakybo/sengine/SEngine.java
@@ -60,7 +60,7 @@ public final class SEngine
 	 * Create the engine, called by the {@link Game#Game(String)}
 	 * @param game The game
 	 */
-	static void create(String gameame)
+	static void create(String gameName)
 	{
 		if(!created)
 		{
