d417a73e6bbda83a1dcc9999ca1b028b8b25d3c8
Kevin Krol <Krol.kevin@gmail.com>
2016-04-14 23:31:05 +0200

    Updated test shader

--- res/shaders/test.glsl
+++ res/shaders/test.glsl
@@ -1,21 +1,33 @@
 #ifdef VERTEX_PASS
 
 layout (location=0) in vec3 position;
+layout (location=1) in vec2 texCoord;
+layout (location=2) in vec3 normal;
+
+out vec2 texCoord0;
+
+uniform mat4 mvp;
 
 void main()
 {
-    gl_Position = vec4(position, 1.0);
+    gl_Position = mvp * vec4(position, 1.0);
+    
+    texCoord0 = texCoord;
 }
 
 #endif
 
 #ifdef FRAGMENT_PASS
 
+in vec2 texCoord0;
+
 out vec4 fragColor;
+
+uniform sampler2D diffuse;
 
 void main()
 {
-    fragColor = vec4(0.0, 0.5, 0.5, 1.0);
+	fragColor = texture2D(diffuse, texCoord0);
 }
 
 #endif
