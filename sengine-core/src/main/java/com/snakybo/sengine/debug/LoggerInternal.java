0fd70ca93d5826b5b63139086f76a49012a5135b
Kevin Krol <Krol.kevin@gmail.com>
2016-04-17 02:14:58 +0200

    Added all log types to LoggerInternal

--- sengine-core/src/main/java/com/snakybo/sengine/debug/LoggerInternal.java
+++ sengine-core/src/main/java/com/snakybo/sengine/debug/LoggerInternal.java
@@ -149,4 +149,263 @@ public final class LoggerInternal
 			Logger.log(o, source);
 		}
 	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(boolean, Object)}, for internal engine use only
+	 * @param b The boolean to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(boolean b, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(b, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(char, Object)}, for internal engine use only
+	 * @param c The char to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(char c, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(c, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(int, Object)}, for internal engine use only
+	 * @param i The int to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(int i, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(i, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(float, Object)}, for internal engine use only
+	 * @param f The float to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(float f, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(f, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(double, Object)}, for internal engine use only
+	 * @param d The double to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(double d, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(d, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(long, Object)}, for internal engine use only
+	 * @param l The long to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(long l, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(l, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(char[], Object)}, for internal engine use only
+	 * @param s The char array to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(char[] s, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(s, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(String, Object)}, for internal engine use only
+	 * @param s The string to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(String s, Object source)
+	{		
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(s, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logWarning(Object, Object)}, for internal engine use only
+	 * @param o The object to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logWarning(Object o, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logWarning(o, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(boolean, Object)}, for internal engine use only
+	 * @param b The boolean to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(boolean b, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(b, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(char, Object)}, for internal engine use only
+	 * @param c The char to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(char c, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(c, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(int, Object)}, for internal engine use only
+	 * @param i The int to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(int i, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(i, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(float, Object)}, for internal engine use only
+	 * @param f The float to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(float f, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(f, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(double, Object)}, for internal engine use only
+	 * @param d The double to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(double d, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(d, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(long, Object)}, for internal engine use only
+	 * @param l The long to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(long l, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(l, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(char[], Object)}, for internal engine use only
+	 * @param s The char array to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(char[] s, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(s, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(String, Object)}, for internal engine use only
+	 * @param s The string to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(String s, Object source)
+	{		
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(s, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logError(Object, Object)}, for internal engine use only
+	 * @param o The object to log
+	 * @param source The source of the message, can also be a string
+	 */
+	public static void logError(Object o, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logError(o, source);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logException(Exception)}, for internal engine use only
+	 * @param e The exception to log
+	 */
+	public static void logException(Exception e)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logException(e);
+		}
+	}
+	
+	/**
+	 * Debug.LOG_DEBUG version of {@link Logger#logException(Exception, Object)}, for internal engine use only
+	 * @param e The exception to log
+	 * @param source The source of the exception, can also be a string
+	 */
+	public static void logException(Exception e, Object source)
+	{
+		if(Debug.LOG_DEBUG)
+		{
+			Logger.logException(e, source);
+		}
+	}
 }
